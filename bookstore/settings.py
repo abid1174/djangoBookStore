ace728c241c0d02b1af6f04ced8074c4d2ffd130
abid413119 <abidastronaut@gmail.com>
2020-04-11 12:51:32 +0600

    Tests for pages/Homepage app

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'user.apps.UserConfig'
+    'user.apps.UserConfig',
+    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
@@ -45,7 +46,7 @@ ROOT_URLCONF = 'bookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ pages/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from .views import HomePageView
+
+
+class HomePageTests(SimpleTestCase):
+    
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, "home.html")
+    
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, "Homepage")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
